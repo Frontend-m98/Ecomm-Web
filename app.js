de0bd6148c9e4ca9e858f2237e970ba0f9c8574f
Frontend-m98 <azizqulovmuhiddin466@gmail.com>
2026-04-24 11:32:06 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+const slider = document.querySelector(".slider");
+
+let index = 0;
+
+function showSlide(i) {
+    // DOT ACTIVE
+    dots.forEach(dot => dot.classList.remove("active"));
+    dots[i].classList.add("active");
+
+    // 🔥 SLIDE
+    slider.style.transform = `translateX(-${i * 100}%)`;
+}
+
+function nextSlide() {
+    index++;
+    if (index >= slides.length) index = 0;
+    showSlide(index);
+}
+
+setInterval(nextSlide, 3000);
+
+// DOT CLICK
+dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => {
+        index = i;
+        showSlide(index);
+    });
+});
+
+
+// topscroll-btn
+
+const btn = document.getElementById("scrollTopBtn");
+
+// scroll bo‘lsa button chiqadi
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// bosilganda tepaga chiqadi
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
